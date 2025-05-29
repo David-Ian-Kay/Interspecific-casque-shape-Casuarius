d2181e3e96647910ef4a2221f3b50cb16f89efbb
David-Ian-Kay <86896685+David-Ian-Kay@users.noreply.github.com>
2025-05-29 17:45:47 -0400

    .RData files and analysis script added via upload
    Uploaded the .RData files of the shape coordinates and the updated analysis R script.

--- CassowaryInterspAnalysis.R
+++ CassowaryInterspAnalysis.R
@@ -6,7 +6,7 @@ library(tibble)
 library(ggplot2)
 library(MASS)
 library(vegan)
-setwd("FILE-PATH/Interspecific-casque-disparity-Casuarius")
+setwd("/CassowaryIntersp/")
 
 ### FUNCTION ###
 euclidean <- function(a, b) sqrt(sum((a - b)^2))
@@ -19,45 +19,52 @@ cas <- which(sp.list %in% "casuarius")
 una <- which(sp.list %in% "unappendiculatus") # smallest sample #
 coord.data <- cass_all_lat_ldk_bk_slid$coo
 
-cal_cass_lat_all_ldk_bk_slid <- calibrate_harmonicpower_efourier(cass_all_lat_ldk_bk_slid, nb.h = 20, plot = T)
-# cal_cass_lat_all_ldk_bk_slid
-cal_cass_lat_all_ldk_bk_slid$gg + theme_minimal() + coord_cartesian(xlim = c(0.5, 17), ylim = c(0, 100)) + ggtitle("Cassowary lateral landmark Bookstein slid Harmonic calibration")
-cal_cass_lat_all_ldk_bk_slid_recon <- calibrate_reconstructions_efourier(cass_all_lat_ldk_bk_slid, range = 1:16)
-cal_cass_lat_all_ldk_bk_slid_recon
-cass_lat_all_ldk_bk_slid_ef <- efourier(cass_all_lat_ldk_bk_slid, nb.h = 16,norm = F)
+cal_cass_lat_all <- calibrate_harmonicpower_efourier(cass_all_lat_ldk_bk_slid, nb.h = 20, plot = T)
+# cal_cass_lat_all
+cal_cass_lat_all$gg + theme_minimal() + coord_cartesian(xlim = c(0.5, 17), ylim = c(0, 100)) + ggtitle("Cassowary Lateral View Harmonic calibration")
+cal_cass_lat_all_recon <- calibrate_reconstructions_efourier(cass_all_lat_ldk_bk_slid, range = 1:16)
+cal_cass_lat_all_recon
+cass_lat_all_ef <- efourier(cass_all_lat_ldk_bk_slid, nb.h = 16,norm = F)
 
-## LDA ##
-cass_lat_all_ldk_bk_slid_MDS_MOMOCS <- MDS(cass_lat_all_ldk_bk_slid_ef, k = 39)
-str(cass_lat_all_ldk_bk_slid_MDS_MOMOCS)
-summary(cass_lat_all_ldk_bk_slid_MDS_MOMOCS)
-plot_MDS(cass_lat_all_ldk_bk_slid_MDS_MOMOCS)
-cass_lat_all_ldk_bk_slid_PCO_MOMOCS <- capscale(cass_lat_all_ldk_bk_slid_ef ~ 1, distance = "euclidean")
+## Principal Coordinates Analysis (PCA) ##
+cass_lat_all_PCA <- PCA(cass_lat_all_ef)
+cass_lat_all_PCA
+summary(cass_lat_all_PCA) # shows to keep 8 PCs to capture >>99% of variation
+pc.shape_lat <- cass_lat_all_PCA$x
 
-
-cass_test_ldk_bk_slid_PCO_MOMOCS_lda <- lda(fac.side ~ MDS1 + MDS2 + MDS3 + MDS4 + MDS5 + MDS6 + MDS7 + MDS8, data = cass_test_ldk_bk_slid_PCO_MOMOCS_dat_fac)
-
-## PCA ##
-cass_lat_all_ldk_bk_slid_PCA <- PCA(cass_lat_all_ldk_bk_slid_ef)
-cass_lat_all_ldk_bk_slid_PCA
-summary(cass_lat_all_ldk_bk_slid_PCA) # shows to keep 8 PCs to capture >>99% of variation
-pc.shape <- cass_lat_all_ldk_bk_slid_PCA$x
-
-## LDA ##
-cass_lat_all_ldk_bk_slid_LDA <- LDA(cass_lat_all_ldk_bk_slid_PCA, "species", retain = 8) # 8 PC axes capure 99% variance
-cass_lat_all_ldk_bk_slid_LDA
-
+## Quadratic Discriminant Analysis (QDA) ##
+# create dataframe of the 8 principal components that capture 99% of the shape
+cass_lat_all_PCs <- cbind(pc.shape_lat[,1:8],cass_lat_all_PCA$fac)
+cass_lat_all_QDA <- qda(species~PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8, data=cass_lat_all_PCs,
+prior=c(1,1,1)/3, CV=T)
+cass_lat_all_QDA
+# Build a table of the classification results
+CV.fac_all <- cass_lat_all_QDA$class
+CV.tab_all <- table(cass_lat_all_PCs[,9],CV.fac_all)
+names(dimnames(CV.tab_all)) <- c('actual','classified')
+CV.correct_all <- sum(diag(CV.tab_all))/sum(CV.tab_all)
+tab_all <- CV.tab_all
+  ce_all <- sapply(seq_along(1:nrow(tab_all)),
+  function(i) 1-(sum(tab_all[i, -i])/sum(tab_all[i, ])))
+  names(ce_all) <- rownames(tab_all)
+# QDA overall classification rate
+CV.correct_all
+# QDA correct classification rate
+ce_all
+#Classification table
+tab_all
 
 ### PERMUTATION ANALYSIS ###
-pc.ben <- pc.shape[ben, 1:8]
+pc.ben <- pc.shape_lat[ben, 1:8]
 mean.ben <- apply(pc.ben, 2, mean)
-pc.cas <- pc.shape[cas, 1:8]
+pc.cas <- pc.shape_lat[cas, 1:8]
 mean.cas <- apply(pc.cas, 2, mean)
-pc.una <- pc.shape[una, 1:8]
+pc.una <- pc.shape_lat[una, 1:8]
 mean.una <- apply(pc.una, 2, mean)
 
  ## bennetti vs. casuarius ###
-a <- pc.cas
-b <- pc.una
+a <- pc.ben
+b <- pc.cas
 rep <- 1000
 diff <- NULL
 p.list <- NULL
@@ -81,7 +88,7 @@ for(i in 1:rep) {
       n <- n + 1
       }
   }
-  p.rep <- n/1000
+  p.rep <- n/(1000+1)
   p.list[i] <- p.rep
   n <- 1
 }
@@ -90,10 +97,80 @@ mean(p.list)
 min(p.list)
 max(p.list)
 
+## bennetti vs. unappendiculatus ###
+a <- pc.ben
+b <- pc.una
+rep <- 1000
+diff <- NULL
+p.list <- NULL
+n = 1
+for(i in 1:rep) {
+ subsamp.size <- min(nrow(a), nrow(b))
+ sub.a <- a[sample(nrow(a), size=subsamp.size),]
+ sub.b <- b[sample(nrow(b), size=subsamp.size),]
+ mean.a <- apply(sub.a, 2, mean)
+ mean.b <- apply(sub.b, 2, mean)
+ mean.diff <- euclidean(mean.a, mean.b)
+ #print(mean.diff)
+ comb <- rbind(sub.a, sub.b)
+ for(j in 1:1000) {
+   rand.seq <- sample(nrow(comb))
+   rand.mean.1 <- apply(comb[rand.seq[1:subsamp.size],], 2, mean)
+   rand.mean.2 <- apply(comb[rand.seq[(subsamp.size+1):(subsamp.size*2)],], 2, mean)
+   rand.diff <- euclidean(rand.mean.1, rand.mean.2)
+   #print(rand.diff)
+   if(rand.diff > mean.diff) {
+     n <- n + 1
+     }
+ }
+ p.rep <- n/(1000+1)
+ p.list[i] <- p.rep
+ n <- 1
+}
+p.list
+mean(p.list)
+min(p.list)
+max(p.list)
+
+## casuarius vs. unappendiculatus ###
+a <- pc.cas
+b <- pc.una
+rep <- 1000
+diff <- NULL
+p.list <- NULL
+n = 1
+for(i in 1:rep) {
+ subsamp.size <- min(nrow(a), nrow(b))
+ sub.a <- a[sample(nrow(a), size=subsamp.size),]
+ sub.b <- b[sample(nrow(b), size=subsamp.size),]
+ mean.a <- apply(sub.a, 2, mean)
+ mean.b <- apply(sub.b, 2, mean)
+ mean.diff <- euclidean(mean.a, mean.b)
+ #print(mean.diff)
+ comb <- rbind(sub.a, sub.b)
+ for(j in 1:1000) {
+   rand.seq <- sample(nrow(comb))
+   rand.mean.1 <- apply(comb[rand.seq[1:subsamp.size],], 2, mean)
+   rand.mean.2 <- apply(comb[rand.seq[(subsamp.size+1):(subsamp.size*2)],], 2, mean)
+   rand.diff <- euclidean(rand.mean.1, rand.mean.2)
+   #print(rand.diff)
+   if(rand.diff > mean.diff) {
+     n <- n + 1
+     }
+ }
+ p.rep <- n/(1000+1)
+ p.list[i] <- p.rep
+ n <- 1
+}
+p.list
+mean(p.list)
+min(p.list)
+max(p.list)
+
 # PCA plot without labels #
-plot_PCA(cass_lat_all_ldk_bk_slid_PCA, "species")
-svg(filename = "./Images/cass_lat_all_ldk_bk_slid_PCA_001.svg")
-plot_PCA(cass_lat_all_ldk_bk_slid_PCA)
+plot_PCA(cass_lat_all_PCA, "species")
+svg(filename = "./cass_lat_all_PCA_001.svg")
+plot_PCA(cass_lat_all_PCA)
 dev.off()
 
 ### < ROSTRAL > ###
@@ -104,33 +181,121 @@ cas <- which(sp.list %in% "casuarius")
 una <- which(sp.list %in% "unappendiculatus") # smallest sample #
 coord.data <- cass_all_ros_ldk_bk_slid$coo
 
-cal_cass_ros_all_ldk_bk_slid <- calibrate_harmonicpower_efourier(cass_all_ros_ldk_bk_slid, nb.h = 20, plot = T)
-# cal_cass_ros_all_ldk_bk_slid
-cal_cass_ros_all_ldk_bk_slid$gg + theme_minimal() + coord_cartesian(xlim = c(0.5, 15), ylim = c(0, 100)) + ggtitle("Cassowary roseral landmark Bookstein slid Harmonic calibration")
-cal_cass_ros_all_ldk_bk_slid_recon <- calibrate_reconstructions_efourier(cass_all_ros_ldk_bk_slid, range = 1:14)
-cal_cass_ros_all_ldk_bk_slid_recon
-cass_ros_all_ldk_bk_slid_ef <- efourier(cass_all_ros_ldk_bk_slid, nb.h = 14,norm = F)
+cal_cass_ros_all <- calibrate_harmonicpower_efourier(cass_all_ros_ldk_bk_slid, nb.h = 20, plot = T)
+# cal_cass_ros_all
+cal_cass_ros_all$gg + theme_minimal() + coord_cartesian(xlim = c(0.5, 15), ylim = c(0, 100)) + ggtitle("Cassowary Rostral View Harmonic calibration")
+cal_cass_ros_all_recon <- calibrate_reconstructions_efourier(cass_all_ros_ldk_bk_slid, range = 1:14)
+cal_cass_ros_all_recon
+cass_ros_all_ef <- efourier(cass_all_ros_ldk_bk_slid, nb.h = 14,norm = F)
 
 ## PCA ##
-cass_ros_all_ldk_bk_slid_PCA <- PCA(cass_ros_all_ldk_bk_slid_ef)
-cass_ros_all_ldk_bk_slid_PCA
-summary(cass_ros_all_ldk_bk_slid_PCA) # shows to keep 8 PCs to capture >>99% of variation
-pc.shape <- cass_ros_all_ldk_bk_slid_PCA$x
+cass_ros_all_PCA <- PCA(cass_ros_all_ef)
+cass_ros_all_PCA
+summary(cass_ros_all_PCA) # shows to keep 8 PCs to capture >>99% of variation
+pc.shape_ros <- cass_ros_all_PCA$x
 
-## LDA ##
-cass_ros_all_ldk_bk_slid_LDA <- LDA(cass_ros_all_ldk_bk_slid_PCA, "species", retain = 9) # 9 PC axes captures 99% variance
-cass_ros_all_ldk_bk_slid_LDA
+## QDA ##
+# create dataframe of the 9 principal components that capture 99% of the shape
+cass_ros_all_PCs <- cbind(pc.shape_ros[,1:9],cass_ros_all_PCA$fac)
+cass_ros_all_QDA <- qda(species~PC1+PC2+PC3+PC4+PC5+PC6+PC7+PC8+PC9,
+data = cass_ros_all_PCs, prior=c(1,1,1)/3, CV=TRUE)
+cass_ros_all_QDA
 
+# Build a table of the classification results
+CV.fac_all <- cass_ros_all_QDA$class
+CV.tab_all <- table(cass_ros_all_PCs[,10],CV.fac_all)
+names(dimnames(CV.tab_all)) <- c('actual','classified')
+CV.correct_all <- sum(diag(CV.tab_all))/sum(CV.tab_all)
+tab_all <- CV.tab_all
+  ce_all <- sapply(seq_along(1:nrow(tab_all)),
+  function(i) 1-(sum(tab_all[i, -i])/sum(tab_all[i, ])))
+  names(ce_all) <- rownames(tab_all)
+# QDA overall classification rate
+CV.correct_all
+# QDA Correct classification rate
+ce_all
+#Classification table
+tab_all
 
 ### PERMUTATION ANALYSIS ###
-pc.ben <- pc.shape[ben, 1:9]
+pc.ben <- pc.shape_ros[ben, 1:9]
 mean.ben <- apply(pc.ben, 2, mean)
-pc.cas <- pc.shape[cas, 1:9]
+pc.cas <- pc.shape_ros[cas, 1:9]
 mean.cas <- apply(pc.cas, 2, mean)
-pc.una <- pc.shape[una, 1:9]
+pc.una <- pc.shape_ros[una, 1:9]
 mean.una <- apply(pc.una, 2, mean)
 
 ## bennetti vs. casuarius ###
+a <- pc.ben
+b <- pc.cas
+rep <- 1000
+diff <- NULL
+p.list <- NULL
+n <- 1
+for(i in 1:rep) {
+  subsamp.size <- min(nrow(a), nrow(b))
+  sub.a <- a[sample(nrow(a), size=subsamp.size),]
+  sub.b <- b[sample(nrow(b), size=subsamp.size),]
+  mean.a <- apply(sub.a, 2, mean)
+  mean.b <- apply(sub.b, 2, mean)
+  mean.diff <- euclidean(mean.a, mean.b)
+  #print(mean.diff)
+  comb <- rbind(sub.a, sub.b)
+  for(j in 1:1000) {
+    rand.seq <- sample(nrow(comb))
+    rand.mean.1 <- apply(comb[rand.seq[1:subsamp.size],], 2, mean)
+    rand.mean.2 <- apply(comb[rand.seq[(subsamp.size+1):(subsamp.size*2)],], 2, mean)
+    rand.diff <- euclidean(rand.mean.1, rand.mean.2)
+    #print(rand.diff)
+    if(rand.diff > mean.diff) {
+      n <- n + 1
+    }
+  }
+  p.rep <- n/(1000+1)
+  p.list[i] <- p.rep
+  n <- 1
+}
+p.list
+mean(p.list)
+min(p.list)
+max(p.list)
+
+## bennetti vs. unappendiculatus ###
+a <- pc.ben
+b <- pc.una
+rep <- 1000
+diff <- NULL
+p.list <- NULL
+n <- 1
+for(i in 1:rep) {
+  subsamp.size <- min(nrow(a), nrow(b))
+  sub.a <- a[sample(nrow(a), size=subsamp.size),]
+  sub.b <- b[sample(nrow(b), size=subsamp.size),]
+  mean.a <- apply(sub.a, 2, mean)
+  mean.b <- apply(sub.b, 2, mean)
+  mean.diff <- euclidean(mean.a, mean.b)
+  #print(mean.diff)
+  comb <- rbind(sub.a, sub.b)
+  for(j in 1:1000) {
+    rand.seq <- sample(nrow(comb))
+    rand.mean.1 <- apply(comb[rand.seq[1:subsamp.size],], 2, mean)
+    rand.mean.2 <- apply(comb[rand.seq[(subsamp.size+1):(subsamp.size*2)],], 2, mean)
+    rand.diff <- euclidean(rand.mean.1, rand.mean.2)
+    #print(rand.diff)
+    if(rand.diff > mean.diff) {
+      n <- n + 1
+    }
+  }
+  p.rep <- n/(1000+1)
+  p.list[i] <- p.rep
+  n <- 1
+}
+p.list
+mean(p.list)
+min(p.list)
+max(p.list)
+
+## casuarius vs. unappendiculatus ###
 a <- pc.cas
 b <- pc.una
 rep <- 1000
@@ -156,7 +321,7 @@ for(i in 1:rep) {
       n <- n + 1
     }
   }
-  p.rep <- n/1000
+  p.rep <- n/(1000+1)
   p.list[i] <- p.rep
   n <- 1
 }
@@ -165,14 +330,13 @@ mean(p.list)
 min(p.list)
 max(p.list)
 
-
-plot_PCA(cass_ros_all_ldk_bk_slid_PCA, "species")
-svg(filename = "./Images/cass_lat_all_ldk_bk_slid_PCA_001.svg")
-plot_PCA(cass_ros_all_ldk_bk_slid_PCA)
+plot_PCA(cass_ros_all_PCA, "species")
+svg(filename = "./cass_lat_all_PCA_001.svg")
+plot_PCA(cass_ros_all_PCA)
 dev.off()
 
 # PCA plot with point labels #
-plot_PCA(cass_ros_all_ldk_bk_slid_PCA, labelpoints = T)
-svg(filename = "./Images/cass_lat_all_ldk_bk_slid_PCA_002.svg")
-plot_PCA(cass_ros_all_ldk_bk_slid_PCA, labelpoints = T)
+plot_PCA(cass_ros_all_PCA, labelpoints = T)
+svg(filename = "./cass_lat_all_PCA_002.svg")
+plot_PCA(cass_ros_all_PCA, labelpoints = T)
 dev.off()
